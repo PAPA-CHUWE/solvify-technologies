7c22a60e22e1b9ddaef8a81e8027f4f35f768931
terrencec <tchuwe41@gmail.com>
2024-03-14 15:34:38 +0200

    final push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,6 @@ import "@fontsource/roboto/700.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import LazyLoadComponent from "./utils/LazyLoadComponent ";
 
 const inter = Inter({ subsets: ["latin"] });
