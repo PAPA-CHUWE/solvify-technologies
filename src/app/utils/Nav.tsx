a91e5ea5411d1bcfc91e016777159630cdb37caa
terrencec <tchuwe41@gmail.com>
2024-03-14 15:40:09 +0200

    final push1

--- src/app/utils/Nav.tsx
+++ src/app/utils/Nav.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faUser,
